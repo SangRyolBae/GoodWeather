6edd39b78cc0fb3d395991a3b8df2774cc54366b
BaeSangRyol <furyterrain@gmail.com>
2020-02-26 12:49:54 +0900

    Initial Commit

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by 배상렬 on 2020/02/26.
+//  Copyright © 2020 BaeSangRyol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
