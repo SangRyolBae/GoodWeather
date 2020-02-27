e17ed302eb1f1e5932fa568d9ed009699a2a997a
배상렬 <furyterrain@blockbase.co.kr>
2020-02-27 11:42:13 +0900

    UI 배치 작업

---
+++ GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AddWeatherCityViewController.swift
+//  GoodWeather
+//
+//  Created by Mohammad Azam on 12/1/18.
+//  Copyright © 2018 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddWeatherCityViewController: UIViewController {
+    
+    @IBOutlet weak var cityNameTextField: UITextField!
+    
+    @IBAction func saveCityButtonPressed() {
+        
+        
+        
+    }
+    
+    @IBAction func close() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
